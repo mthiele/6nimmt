b5ee4096d8b1c7661af4012b2569cda13effee7a
mthiele <thiele.michael2@googlemail.com>
2020-05-26 09:54:16 +0200

    added basic monitoring (prometheus + grafana)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("io.micrometer:micrometer-registry-prometheus")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
